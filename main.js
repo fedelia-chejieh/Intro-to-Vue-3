d3d9543bd9446f0e3255a071228b700990c367a8
Fedelia Chejieh <fedelia.cj@gmail.com>
2024-05-11 15:02:50 +0100

    Added image and link

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: `./assets/images/socks_green.jpg`,
+      URL: `https://s3.amazonaws.com/shecodesio-production/uploads/files/000/116/782/original/karsten-winegeart-b-i8TPBVslM-unsplash.jpg?1708983481`,
+    };
+  },
+});
